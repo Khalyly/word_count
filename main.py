d6d18d73771ccfcfc8104c826f5c10559c0c6c49
Khalyly <143745931+Khalyly@users.noreply.github.com>
2023-09-19 11:40:31 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+def count_word(s):
+    countOfWords = len(s.split())
+    return countOfWords
+
+
+print(count_word('siena kys'))
+
+#J'ai fait un nouveau projet et je l'ai nommé word_count
+#J'ai mit un fichier et je l'ai nommé main.py
+#J'ai fait une recherche sur Internet pour faire une fonction et je l'ai nommé count_word
+#J'ai fait une recherche sur Internet pour trouver comment une fonction reçoit un argument de type string et je l'ai fait
+#J'ai fait une recherche sur Internet pour trouver comment retourner le nombre de mots dans une fonction et je l'ai fait
